ae90c06c3c41a2fee86db225013a1471d5989143
Vladimir2090 <zvladzubkov@gmail.com>
2025-08-31 19:05:07 +0300

    add read torrent file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main(){
+use lava_torrent::torrent::v1::Torrent;
+use hex::encode;
+use std::error::Error;
+
+fn main() {
+    if let Err(e) = reader_torrent() {
+        println!("[ERROR] {}", e);
+    }
+}
+
+fn reader_torrent() -> Result<(), Box<dyn Error>> {
+    let torrent = Torrent::read_from_file(r"C:\Users\admin\Desktop\torrent_folder\yourfile.torrent")?;
     
+    let announce = torrent.announce.as_ref().ok_or("[ERROR] announce not found")?;
+    println!("[MSG] announce: {}", announce);
+
+    let info_hash = torrent.info_hash();
+    println!("[MSG] info hash (hex): {}", encode(info_hash));
+
+    Ok(())
 }
