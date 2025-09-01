575e69c9714b635c0c1dd5e6c6370e6f3917c03d
Vladimir2090 <zvladzubkov@gmail.com>
2025-09-01 09:53:15 +0300

    add config

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,33 @@
 use lava_torrent::torrent::v1::Torrent;
 use hex::encode;
 use std::error::Error;
+use std::fs::File;
+use std::io::BufReader;
+use serde::Deserialize;
 
-fn main() {
-    if let Err(e) = reader_torrent() {
-        println!("[ERROR] {}", e);
-    }
+#[derive(Deserialize)]
+
+struct Config {
+    path_torrent: String,
 }
 
-fn reader_torrent() -> Result<(), Box<dyn Error>> {
-    let torrent = Torrent::read_from_file(r"C:\Users\admin\Desktop\torrent_folder\yourfile.torrent")?;
-    
+fn main() -> Result<(), Box<dyn Error>> {
+    let file = File::open("config.json")?;
+    let reader = BufReader::new(file);
+    let config: Config = serde_json::from_reader(reader)?;
+
+    println!("{}", config.path_torrent);
+
+    if let Err(e) = reader_torrent(&config.path_torrent) {
+        println!("[ERROR] {}", e);
+    }
+
+    Ok(())
+}
+
+// Теперь функция корректно принимает путь по ссылке
+fn reader_torrent(path: &str) -> Result<(), Box<dyn Error>> {
+    let torrent = Torrent::read_from_file(path)?;
     let announce = torrent.announce.as_ref().ok_or("[ERROR] announce not found")?;
     println!("[MSG] announce: {}", announce);
 
